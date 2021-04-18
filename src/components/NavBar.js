3ea6f0631299b21c8e63c6efc8556b0dd7769c53
Ivan Duranic <ivan.duranic@gmail.com>
2021-04-18 16:59:45 -0400

    made SinglePost page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,7 +20,7 @@ export default function NavBar() {
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
             activeClassName="text-red-100 bg-red-700"
           >
-            Blog Posts
+            LinkedIn Posts
           </NavLink>
           <NavLink
             to="/project"
--- src/components/Post.js
+++ src/components/Post.js
@@ -29,10 +29,10 @@ export default function Post() {
     <main className="bg-green-100 min-h-screen p-12">
       <section className="container mx-auto">
         <h1 className="text-5xl flex justify-center cursive">
-          Blog Posts Page
+          LinkedIn Posts Page
         </h1>
         <h2 className="text-lg text-gray-600 flex justify-center mb-12">
-          Welcome to my page of blog posts
+          Welcome to my page of LinkedIn posts
         </h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {postData &&
