9960e40104abdc0bda89ae46d846afb3759f2620
Ivan Duranic <ivan.duranic@gmail.com>
2021-04-19 00:58:23 -0400

    styled the contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "../contact.css";
 import { db } from "../firebase";
 
 const Contact = () => {
@@ -13,19 +12,20 @@ const Contact = () => {
     e.preventDefault();
     setLoader(true);
 
-    db.collection("contacts").add({
-      name: name,
-      email: email,
-      message: message,
-    })
-    .then(() => {
+    db.collection("contacts")
+      .add({
+        name: name,
+        email: email,
+        message: message,
+      })
+      .then(() => {
         alert("Message has been submitted 👍");
         setLoader(false);
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         alert(error.message);
         setLoader(false);
-    });
+      });
 
     setName("");
     setEmail("");
@@ -33,32 +33,62 @@ const Contact = () => {
   };
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <h1>Contact form ☎</h1>
+    <div className="flex items-center h-screen w-full bg-teal-lighter">
+      <div className="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
+        <h2 className="block w-full text-center text-grey-darkest mb-6">
+          Toronto, ON | Phone: +1 (437) 774-6800 | Email: ivan.duranic@gmail.com
+        </h2>
 
-      <label>Name</label>
-      <input
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      ></input>
-
-      <label>Email</label>
-      <input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      ></input>
-
-      <label>Message</label>
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      ></textarea>
-
-      <button type="submit" style={{background : loader ? "#ccc" : "rgb(2, 2, 110)" }}>Submit</button>
-    </form>
+        <h1 className="block w-full text-center text-grey-darkest mb-6">
+          <strong>Contact form</strong>📲
+        </h1>
+        <form
+          className="mb-4 md:flex md:flex-wrap md:justify-between"
+          onSubmit={handleSubmit}
+        >
+          <div className="field-group mb-4 md:w-1/2">
+            <label className="mb-2 uppercase tracking-wide font-bold text-lg text-grey-darkest">
+              Name
+            </label>
+            <input
+              className="border py-2 px-3 text-grey-darkest md:mr-2"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </div>
+          <div className="flex flex-col mb-4 md:w-full">
+            <label className="mb-2 uppercase font-bold text-lg text-grey-darkest">
+              Email
+            </label>
+            <input
+              className="border py-2 px-3 text-grey-darkest"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </div>
+          <div className="flex flex-col mb-6 md:w-full">
+            <label className="mb-2 uppercase font-bold text-lg text-grey-darkest">
+              Message
+            </label>
+            <textarea
+              className="border py-2 px-3 text-grey-darkest"
+              placeholder="Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </div>
+          <button
+            className="block bg-teal hover:bg-teal-dark text-white uppercase text-lg mx-auto p-4 rounded"
+            type="submit"
+            style={{ background: loader ? "#ccc" : "rgb(2, 2, 110)" }}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer class="h-10 text-center py-2 bg-red-600">Copyright@2021</footer>;
+  return <footer class="w-full text-center border-t border-grey p-4 pin-b bg-red-600">Copyright@2021</footer>;
 }
